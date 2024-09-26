68e22bdf08f00ab93a56e60c35bb93f52d29c799
Bryan Trang <bryanltrang@gmail.com>
2024-09-25 22:30:24 -0700

    setup login and signup routes and make it work with clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,17 +2,25 @@ import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import "react-native-reanimated";
+} from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Slot } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { ConvexReactClient } from "convex/react";
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { ConvexReactClient } from 'convex/react';
+import { ClerkProvider, ClerkLoaded, useAuth } from '@clerk/clerk-expo';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import * as SecureStore from 'expo-secure-store';
+import SpaceMonoRegular from '../assets/fonts/SpaceMono-Regular.ttf';
+
+export interface TokenCache {
+  getToken: (key: string) => Promise<string | undefined | null>;
+  saveToken: (key: string, token: string) => Promise<void>;
+  clearToken?: (key: string) => void;
+}
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -22,9 +30,43 @@ const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
 });
 
 export default function RootLayout() {
+  const tokenCache = {
+    async getToken(key: string) {
+      try {
+        const item = await SecureStore.getItemAsync(key);
+        if (item) {
+          console.log(`${key} was used 🔐 \n`);
+        } else {
+          console.log('No values stored under key: ' + key);
+        }
+        return item;
+      } catch (error) {
+        console.error('SecureStore get item error: ', error);
+        await SecureStore.deleteItemAsync(key);
+        return null;
+      }
+    },
+    async saveToken(key: string, value: string) {
+      try {
+        return SecureStore.setItemAsync(key, value);
+      } catch (err) {
+        console.error('SecureStore set item error: ', err);
+        return;
+      }
+    },
+  };
+
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  if (!publishableKey) {
+    throw new Error(
+      'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+    );
+  }
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: SpaceMonoRegular,
   });
 
   useEffect(() => {
@@ -38,20 +80,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ClerkProvider
-      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
-    >
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        <ThemeProvider
-          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        >
-          <Stack initialRouteName="index">
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-        </ThemeProvider>
-      </ConvexProviderWithClerk>
-    </ClerkProvider>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+        <ClerkLoaded>
+          <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+            <Slot />
+          </ConvexProviderWithClerk>
+        </ClerkLoaded>
+      </ClerkProvider>
+    </ThemeProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,27 @@
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { SignInButton, UserButton } from "@clerk/clerk-react";
-import { Authenticated, Unauthenticated } from "convex/react";
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { SignedIn, SignedOut, useUser } from '@clerk/clerk-react';
+import { Link } from 'expo-router';
 
 export default function Login() {
+  const { user } = useUser();
   return (
     <ThemedView>
-      <ThemedText type="title">Welcome</ThemedText>
-      <Unauthenticated>
+      <ThemedText type="title">Welcome to Hashipetto</ThemedText>
+      <SignedOut>
         <ThemedText type="title">Sign up!</ThemedText>
-        <SignInButton mode="modal" />
-      </Unauthenticated>
-      <Authenticated>
-        <UserButton />
-        You're logged in
-      </Authenticated>
+        <Link href="/(auth)/sign-up">
+          <ThemedText>Create an account</ThemedText>
+        </Link>
+        <Link href="/(auth)/sign-in">
+          <ThemedText>Log In</ThemedText>
+        </Link>
+      </SignedOut>
+
+      <SignedIn>
+        {/* this is where you would show tabs and navigation because the user is logged in */}
+        <ThemedText>Hello {user?.emailAddresses[0].emailAddress}</ThemedText>
+      </SignedIn>
     </ThemedView>
   );
 }
